809efd9b0cd8bb62ab3858bb846f45cb6577aba8
darthlilac <dessalr@live.com>
2024-05-16 11:40:14 -0300

    crud categoria

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,9 +2,18 @@ function Navbar() {
     return (
         <div className="bg-red-800  p-4 border-b-2 border-white">
             <ol className="flex flex-row justify-around">
+            <Link to={"/home"}>
+
                 <li>Farmacia Feliz</li>
+            </Link>
+
+            <Link to={"/cadastroCategoria"}>
                 <li>Cadastro</li>
-                <li>Listagem de Produtos</li>
+            </Link>
+
+            <Link to={"/categorias"}>
+                <li >Listagem</li>
+            </Link>
             </ol>
 
         </div>
