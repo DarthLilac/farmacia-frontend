a2c5a14b3de0bbb39d4f9f602211914b25a71e28
darthlilac <dessalr@live.com>
2024-05-16 11:08:20 -0300

    Componentes - Home, Navbar e Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <div className="bg-red-800 text-center border-white border-t-2 p-7">
+          
+            <ol className="flex justify-around ">
+                <p>©️ 2024</p>
+                <p>| Termos de Serviço  |  Politica de privacidade | </p>
+            </ol>
+
+
+        </div>
+    )
+}
+export default Footer
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className="w-full flex p-5 h-full mt-8">
+            <div>
+                <h1 className="text-6xl mb-8">Farmacia </h1>
+                <p>Seja Bem-Vindo a Farmacia Feliz!</p>
+            </div>
+            <img  className="w-1/2" src="https://cdn.pixabay.com/photo/2024/02/10/17/28/woman-8565237_1280.png"></img>
+        </div>
+    )
+}
+export default Home;
